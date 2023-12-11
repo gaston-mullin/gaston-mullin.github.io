bda6df4e664d07fd403d322d88bfbfe25765e1dc
Gastón Mullin <91540011+gmullin-tiu@users.noreply.github.com>
2023-12-11 01:32:06 +0100

    Add files via upload

--- files/Day 3 -DynamicGames.jl
+++ files/Day 3 -DynamicGames.jl
@@ -1,4 +1,4 @@
-using LinearAlgebra, Plots, DataFrames, SpecialFunctions, Random, GLM
+using LinearAlgebra, Plots, DataFrames, SpecialFunctions, Random, GLM, Optim
 
 ## GENERATE DATA
 
@@ -10,7 +10,7 @@ S=5;
 Xn=10;
 β=.9;
 
-#structural entry coefficients theta:
+#Structural entry coefficients theta:
 bx=-.05;
 bx0=0;
 bnf=-.2;
@@ -38,25 +38,20 @@ nf=(0:Nf)';
 
 state_args=(0:Nf,0:Xn-1,0:S-1,0:1);
 
-NC=4;
-ii = NC:-1:1;
-
-A = DataFrame(Iterators.product(state_args[1],state_args[2],state_args[3],state_args[4]));
-A = Matrix(A);
-#A2 = vec(collect(Iterators.product(state_args[1],state_args[2],state_args[3],state_args[4])));
+A = Matrix(DataFrame(Iterators.product(state_args[1],state_args[2],state_args[3],state_args[4])));
 
 # Fill binomial
 
-bine=zeros(Nf+1,Nf+1);
+binom=zeros(Nf+1,Nf+1);
 for n in 1:Nf+1   
     for k in 1:n       
-        bine[n,k]=binomial(n-1,k-1);                  
+        binom[n,k]=binomial(n-1,k-1);                  
     end        
 end
 
 ## Generate data
 
-theta=[bnf, bx, bss, be]';
+θ=[bnf, bx, bss, be]';
 Util=zeros(size(A,1),1);
 for j in 1:size(A,1)
     Util[j]=0+bx*A[j,2]+bnf*A[j,1]+bss*A[j,3]+be*(1-A[j,4]);
@@ -64,13 +59,125 @@ end
 
 N=Nf;
 
+# FUNCTIONS
+
+function nfirms(pe1,pi1,bine,bini,ne,N)
+
+    Pe=zeros(ne+1,1);
+    for j in 1:ne+1
+        Pe[j]=bine[j]*(pe1^(j-1))*((1-pe1)^(ne-(j-1)));
+    end
+    
+    Pi=zeros(N-ne+1,1);
+    for j in 1:N-ne+1
+        Pi[j]=bini[j]*(pi1^(j-1))*((1-pi1)^(N-ne-(j-1)));
+    end
+    
+    BigP=zeros(1,N+1);
+    j=0;
+    
+    while j<ne+1
+        k=0;
+        while k<N-ne+1
+            BigP[j+k+1]=Pe[j+1]*Pi[k+1]+BigP[j+k+1];
+            k=k+1;
+        end
+        j=j+1;
+    end
+    return BigP;
+end
+
+function prob_entry(Util,trans,N,Xn,S,bine,β,A)
+    BigP=zeros(size(A,1),N+1);
+    fv=zeros(size(A,1),1);
+    eul=Base.MathConstants.eulergamma;
+    
+    x1 = (1 .+ exp.(Util));
+    p=exp.(Util)./(1 .+ exp.(Util));
+    
+    p0=zeros((N+1)*Xn*S*2,1);
+    p2=p;
+
+    while maximum(abs.(p0-p2))>.0000000001
+        p0=copy(p2);
+
+        for j in 1:size(A,1)
+            ind1= only(findall(row -> row == [minimum([A[j,1]+A[j,4],N]),A[j,2],A[j,3],0], eachrow(A)));
+            ind2= only(findall(row -> row == [maximum([A[j,1]+A[j,4]-1,0]),A[j,2],A[j,3],1], eachrow(A)));
+            BigP[j,:]=nfirms(p[ind1],p[ind2],bine[N-A[j,1]+1,:],bine[A[j,1]+1,:],N-A[j,1],N);
+    
+            v=0;
+            for s2=1:S
+                toLookIntoP = hcat(collect(0:5),repeat([A[j,2] s2-1 1],outer = [6,1]));
+                elementsP = [sum(row) >= 1 for row in eachrow([all(row_a .== row_b) for row_a in eachrow(A), row_b in eachrow(toLookIntoP)])];
+                v=v + trans[A[j,3]+1,s2]*(BigP[j,:]'*log.(1 .- p[elementsP]));
+            end
+            fv[j]=-v;
+        
+            toLookIntoUtil = hcat(collect(0:5),repeat([A[j,2] A[j,3] A[j,4]],outer = [6,1]))
+            elementsUtil = [sum(row) >= 1 for row in eachrow([all(row_a .== row_b) for row_a in eachrow(A), row_b in eachrow(toLookIntoUtil)])];
+            tu=BigP[j,:]'*Util[elementsUtil]-β*v+β*eul;
+            p[j]=exp(tu)/(1+exp(tu));
+    
+        end
+        p2=p;
+    end
+    fv=β.*(fv .+ eul);
+    return p,BigP,fv;
+end
 
 # Find the fixed point that characterizes the equilibrium.
 
-#out = prob_entry(Util,trans,Nf,Xn,S,bine,β,A);
-prob_out,h2,h3 = prob_entry(Util,trans,Nf,Xn,S,bine,β,A);
+prob_out,h2,h3 = prob_entry(Util,trans,Nf,Xn,S,binom,β,A);
 
 Random.seed!(2023);
+
+# FUNCTIONS
+
+function EntryDataGen(p,ctrans,S,Xn,Nf,Nm,T,bp,Tl,A)
+    Firm=zeros(Nm,T+Tl,Nf);
+    Lfirm=zeros(Nm,T+Tl+1,Nf);
+    X = rand(0:Xn-1,Nm);
+    State=zeros(Nm,T+Tl+1);
+    Y=zeros(Nm,T+Tl);
+    
+    State[:,1]=rand(1:S,Nm);
+    
+    Draw1=rand(Nm,T+Tl,Nf);
+    Draw2=rand(Nm,T+Tl);
+    Draw3=randn(Nm,T+Tl);
+    
+    for nm in 1:Nm
+        Nfirm=0;
+        for t in 1:T+Tl
+            for nf in 1:Nf
+                rowOfA = [Nfirm-Lfirm[nm,t,nf],X[nm],State[nm,t]-1,Lfirm[nm,t,nf]];
+                ind = [findfirst(row -> row == rowOfA, eachrow(A))];
+                Firm[nm,t,nf]=p[only(ind)]>Draw1[nm,t,nf];
+            end
+    
+            Nfirm=sum(Firm[nm,t,:]);
+            Lfirm[nm,t+1,:]=Firm[nm,t,:];
+    
+            Y[nm,t]=only([1 Nfirm X[nm]-1 State[nm,t]-1]*bp) + Draw3[nm,t];
+
+            State[nm,t+1]=1;
+    
+            for s=1:S-1
+                State[nm,t+1]=State[nm,t+1]+(Draw2[nm,t]>ctrans[Int(State[nm,t]),s]);
+            end
+        end
+    end
+    
+    Firm=Firm[:,Tl+1:T+Tl,:];
+    State=State[:,Tl+1:T+Tl] .- 1;
+    Lfirm=Lfirm[:,Tl+1:T+Tl,:];
+    Y=Y[:,Tl+1:T+Tl];
+
+    return Firm, X, State, Y, Lfirm
+end
+    
+
 
 # Using the equilibrium choice probabilities, simulate the data on firm
 # choices and states.
@@ -108,120 +215,70 @@ B = DataFrame(A,["LNFirms","X","S","LFirm"]);
 B_fit = [ones(size(B,1),1) B.X (B.X./10).^2 B.LFirm B.LNFirms (B.LNFirms./5).^2 B.S B.S.*B.X./10 B.LFirm.*B.S B.S.*B.LNFirms./10];
 ccp_hat=predict(model1,B_fit);
 
+## SECOND STAGE 
 
-#### FUNCTIONS
+γ = Base.MathConstants.eulergamma;
 
-function nfirms(pe1,pi1,bine,bini,ne,N)
+p=ccp_hat;
+bigp=zeros(size(A,1),N+1);
+fv=zeros(size(A,1),1);
+# Iterate through each of the states
+for j in 1:size(A,1)
 
-    Pe=zeros(ne+1,1);
-    for j in 1:ne+1
-        Pe[j]=bine[j]*(pe1^(j-1))*((1-pe1)^(ne-(j-1)));
-    end
-    
-    Pi=zeros(N-ne+1,1);
-    for j in 1:N-ne+1
-        Pi[j]=bini[j]*(pi1^(j-1))*((1-pi1)^(N-ne-(j-1)));
-    end
-    
-    BigP=zeros(1,N+1);
-    j=0;
-    
-    while j<ne+1
-        k=0;
-        while k<N-ne+1
-            BigP[j+k+1]=Pe[j+1]*Pi[k+1]+BigP[j+k+1];
-            k=k+1;
-        end
-        j=j+1;
-    end
-    return BigP;
-end
+    # Identify which states are (potential) new entrants (ind1) and which
+    # are incumbents (ind2):
 
+    ind1 = only(findall(row -> row == [minimum([A[j,1]+A[j,4],N]),A[j,2],A[j,3],0], eachrow(A)));
+    ind2 = only(findall(row -> row == [maximum([A[j,1]+A[j,4]-1,0]),A[j,2],A[j,3],1], eachrow(A)));
+    
+    # Step 1: calculate BigP for state z(j)
+    pe1=p[ind1]; # Choice probability for new entrants
+    pi1=p[ind2]; # Choice probability for incumbents
 
-function prob_entry(Util,trans,N,Xn,S,bine,Beta,A)
-    BigP=zeros(size(A,1),N+1);
-    fv=zeros(size(A,1),1);
-    eul=Base.MathConstants.eulergamma;
-    
-    x1 = (1 .+ exp.(Util));
-    p=exp.(Util)./(1 .+ exp.(Util));
-    
-    p0=zeros((N+1)*Xn*S*2,1);
-    p2=p;
+    ne=N-B.LNFirms[j];
 
-    while maximum(abs.(p0-p2))>.0000000001
-        p0=p2;
+    bine_temp=binom[ne+1,:];
+    bini_temp=binom[B.LNFirms[j]+1,:];
 
-        for j in 1:size(A,1)
-            ind1= only(findall(row -> row == [minimum([A[j,1]+A[j,4],N]),A[j,2],A[j,3],0], eachrow(A)));
-            ind2= only(findall(row -> row == [maximum([A[j,1]+A[j,4]-1,0]),A[j,2],A[j,3],1], eachrow(A)));
-            BigP[j,:]=nfirms(p[ind1],p[ind2],bine[N-A[j,1]+1,:],bine[A[j,1]+1,:],N-A[j,1],N);
+    # Number of firm transition combinations times the probability of each
+    # combination, separated by entrants and incumbents:
+    Pe=bine_temp[1:ne+1].*(pe1.^((1:ne+1) .- 1)).*((1-pe1).^(ne .- ((1:ne+1) .- 1))); 
+    Pi=bini_temp[1:N-ne+1].*(pi1.^((1:N-ne+1) .- 1)).*((1-pi1).^(N-ne .-((1:N-ne+1) .- 1)));
     
-            v=0;
-            for s2=1:S
-                toLookIntoP = hcat(collect(0:5),repeat([A[j,2] s2-1 1],outer = [6,1]));
-                elementsP = [sum(row) >= 1 for row in eachrow([all(row_a .== row_b) for row_a in eachrow(A), row_b in eachrow(toLookIntoP)])];
-                v=v + trans[A[j,3]+1,s2]*(BigP[j,:]'*log.(1 .- p[elementsP]));
-            end
-            fv[j]=-v;
-        
-            toLookIntoUtil = hcat(collect(0:5),repeat([A[j,2] A[j,3] A[j,4]],outer = [6,1]))
-            elementsUtil = [sum(row) >= 1 for row in eachrow([all(row_a .== row_b) for row_a in eachrow(A), row_b in eachrow(toLookIntoUtil)])];
-            tu=BigP[j,:]'*Util[elementsUtil]-Beta*v+Beta*eul;
-            p[j]=exp(tu)/(1+exp(tu));
-    
-        end
-        p2=p;
-    end
-    fv=Beta.*(fv .+ eul);
-    return p,BigP,fv;
-end
-
-## function below not updated
-
-function EntryDataGen(p,ctrans,S,Xn,Nf,Nm,T,bp,Tl,A)
-    Firm=zeros(Nm,T+Tl,Nf);
-    Lfirm=zeros(Nm,T+Tl+1,Nf);
-    X = rand(0:Xn-1,Nm);
-    State=zeros(Nm,T+Tl+1);
-    Y=zeros(Nm,T+Tl);
-    
-    State[:,1]=rand(1:S,Nm);
-    
-    Draw1=rand(Nm,T+Tl,Nf);
-    Draw2=rand(Nm,T+Tl);
-    Draw3=randn(Nm,T+Tl);
-    
-    for nm in 1:Nm
-        Nfirm=0;
-        for t in 1:T+Tl
-            for nf in 1:Nf
-                ind = [findfirst(row -> row == [Nfirm-Lfirm[nm,t,nf],X[nm],State[nm,t]-1,Lfirm[nm,t,nf]], eachrow(A))]
-                Firm[nm,t,nf]=p[only(ind)]>Draw1[nm,t,nf];
-            end
-    
-            Nfirm=sum(Firm[nm,t,:]);
-            Lfirm[nm,t+1,:]=Firm[nm,t,:];
-    
-            Y[nm,t]=only([1 Nfirm X[nm]-1 State[nm,t]-1]*bp) + Draw3[nm,t];
-
-            State[nm,t+1]=1;
-    
-            for s=1:S-1
-                State[nm,t+1]=State[nm,t+1]+(Draw2[nm,t]>ctrans[Int(State[nm,t]),s]);
-            end
+    BigP_temp=zeros(1,N+1);
+    for i=0:ne
+        for k=0:N-ne
+            BigP_temp[i+k+1]=Pe[i+1]*Pi[k+1] + BigP_temp[i+k+1];
         end
     end
-    
-    Firm=Firm[:,Tl+1:T+Tl,:];
-    State=State[:,Tl+1:T+Tl] .- 1;
-    Lfirm=Lfirm[:,Tl+1:T+Tl,:];
-    Y=Y[:,Tl+1:T+Tl];
 
-    return Firm, X, State, Y, Lfirm
+    bigp[j,:]=BigP_temp;
+
+    # Steps 2 and 3: calculate transition probabilities and dynamic
+    # adjustment term fv
+    v=0;
+    for s2=1:S
+        toLookIntoAv = hcat(collect(0:5),repeat([A[j,2] s2-1 1],outer = [6,1]))
+        elementsOfAv = [sum(row) >= 1 for row in eachrow([all(row_a .== row_b) for row_a in eachrow(A), row_b in eachrow(toLookIntoAv)])];
+        v=v+trans[B.S[j]+1,s2]*bigp[j,:]'*log.(1 .- p[elementsOfAv]);
+    end
+    fv[j]=-v;
+
 end
-    
 
+fv=β*(fv .+ γ);
 
+# Match state realizations in the data to rows of A, fv, and bigp:
 
-####
+toLookState = hcat(LNFirm_vec,Z[:,2],Z[:,3],LFirm_vec);
+indicesOfState = indexin(collect(eachrow(toLookState)),collect(eachrow(A)));
+
+FV=fv[indicesOfState];
+BigP=bigp[indicesOfState,:];
+
+Z[:,end] = BigP*nf';
+# Defining the likelihood: a logit likelihood as before
+LikeFun = b -> sum(log.(1 .+ exp.(Z*b+FV))-Firm_vec.*(Z*b+FV));
+
+result = optimize(LikeFun, 0.1*ones(5,1), LBFGS(); autodiff = :forward);
+
